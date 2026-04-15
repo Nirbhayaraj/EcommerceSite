196226be95bde02c340066a54ad79b307e1f2e89
Nirbhay raj <9534990436nirbhaya@gmail.com>
2026-04-16 02:06:56 +0530

    Update constants.js

--- admin/src/config/constants.js
+++ admin/src/config/constants.js
@@ -1,3 +1,5 @@
-export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const rawBackendUrl = import.meta.env.VITE_BACKEND_URL || '';
+
+export const backendUrl = rawBackendUrl.replace(/\/+$/, '');
 export const currency = '$';
 export const ADMIN_TOKEN_KEY = 'adminToken';
